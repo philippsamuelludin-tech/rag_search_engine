a3f8f821e28f4462db0de93dfd599899eccc573d
philippsamuelludin-tech <philipp.samuel.ludin@gmail.com>
2026-06-22 17:38:55 +0200

    Refactor code structure for improved readability and maintainability

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,14 +1,28 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
 
     def gernerate_embedding(self, text):
         if text.isspace():
             raise ValueError("text input was empty")
         embedding = self.model.encode([text])
         return embedding[0]
+    
+    def build_embeddings(self, documents):
+        self.documents = documents
+        doc_list = []
+        for doc in documents:
+            self.document_map[doc["id"]] = doc
+            doc_list.append(f"{doc['title']}: {doc['description']}")
+        self.embeddings = self.model.encode(doc_list, show_progress_bar=True)
+        np.save("cache/movie_embeddings.npy", self.embeddings)
+        return self.embeddings
 
 def verify_model():
     search = SemanticSearch()
